04ab1a5262540d855477035f598bf5930e76f6a1
Maria Zinkevich <zinkevichmaryia@gmail.com>
2021-08-26 00:49:26 +0300

    feat: useReducer in addItem component

--- todo/src/Components/AddItem.jsx
+++ todo/src/Components/AddItem.jsx
@@ -1,46 +1,64 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import { addTodo } from "../Requests";
+import { todos } from "../server/todos";
 import styles from "./addItem.module.css";
 
 //Material-UI
 import { Button } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
 
-let textInput = createRef();
+export const initialState = {
+  item: "",
+  userId: null,
+  check: false,
+  id: null,
+};
 
-function initState({ userId }) {
-    return {
-      item: "",
-      userId,
-      check: false,
-    };
-}
-
-async function reducer(state, action) {
-    if (action.item !== "") {
-      await addTodo(action.item, state.check, state.userId);
-      action.updateDetector();
-      action.setItem(action.item);
-    }
-  }
+export const reducer = (state, action) => {
+  console.log(state, action);
+  todos.push({
+    todo: action.todo,
+    isChecked: state.check,
+    userId: action.userId,
+    id: action.id,
+  });
+  console.log(todos);
+};
 
 export const AddItem = ({ updateDetector, userId }) => {
   const [item, setItem] = useState("");
   const [check, setCheck] = useState(false);
-  const [state, dispatch] = useReducer(reducer, { userId }, initState);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    if (item !== "") {
+      dispatch({
+        todo: item,
+        isChecked: check,
+        userId: userId,
+        id: ++todos[todos.length - 1].id,
+      });
+    }
+    setItem("");
+  };
 
   return (
-    <form onSubmit={() => dispatch({ item, updateDetector, setItem })} id="form" className={`${styles.form}`}>
+    <form
+      onSubmit={() => dispatch({ item, updateDetector })}
+      id="form"
+      className={`${styles.form}`}
+    >
       <TextField
-        ref={textInput}
         label="What should be done?"
         variant="outlined"
         value={item}
-        onChange={(event) => setItem(event.target.value)}
+        onChange={(e) => setItem(e.target.value)}
       />
       <Button
         type="submit"
-        onClick={() => dispatch({ item, updateDetector, setItem })}
+        onClick={submit}
         variant="contained"
         color="primary"
       >
---
+++ todo/src/server/todos.js
@@ -0,0 +1,26 @@
+export const todos = [
+  {
+    todo: "купить продукты",
+    isChecked: true,
+    userId: 1,
+    id: 1,
+  },
+  {
+    todo: "купить кота",
+    isChecked: true,
+    userId: 1,
+    id: 2,
+  },
+  {
+    todo: "купить книгу",
+    isChecked: false,
+    userId: 1,
+    id: 3,
+  },
+  {
+    todo: "купить очки",
+    isChecked: false,
+    userId: 1,
+    id: 4,
+  },
+];
