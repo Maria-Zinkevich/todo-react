12f78c297e0fb055d0330e377e412b1eb6cc5f44
Maria Zinkevich <zinkevichmaryia@gmail.com>
2021-08-26 01:50:55 +0300

    feat: add router to the filter component

--- todo/src/Components/Filter.jsx
+++ todo/src/Components/Filter.jsx
@@ -1,18 +1,57 @@
+import { useEffect } from "react";
 import styles from "./filter.module.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export const Filter = ({ setFilter }) => {
+  function All() {
+    useEffect(() => {
+      setFilter(null);
+    });
+
+    return null;
+  }
+
+  function Сompleted() {
+    useEffect(() => {
+      setFilter(true);
+    });
+
+    return null;
+  }
+
+  function Uncompleted() {
+    useEffect(() => {
+      setFilter(false);
+    });
+
+    return null;
+  }
+
   return (
-    <>
-      <select
-        className={`${styles.select}`}
-        onChange={(e) => {
-          setFilter(e.target.value);
-        }}
-      >
-        <option value="null">All</option>
-        <option value="true">Сompleted</option>
-        <option value="false">Not completed</option>
-      </select>
-    </>
+    <Router>
+      <ul className={`${styles.filterList}`}>
+        <li>
+          <Link to="/">All tasks</Link>
+        </li>
+        <li>
+          <Link to="/completed">Сompleted tasks</Link>
+        </li>
+        <li>
+          <Link to="/uncompleted">Uncompleted tasks</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path="/">
+          <All />
+        </Route>
+        <Route path="/completed">
+          <Сompleted />
+        </Route>
+        <Route path="/uncompleted">
+          <Uncompleted />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
